d3fae83d23e246a618562990f25a9f22f8689f21
MiaTian1024 <sddxtm123@gmail.com>
2024-11-28 12:37:03 +1300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
     AWS_BUCKET_NAME: str
-    AWS_REGION: str = "us-east-1"
+    AWS_REGION: str = "ap-southeast-2"
 
     class Config:
         env_file = ".env"

2d0d3d2c0fbfcc1599761cb1d6ed603aa28c3903
MiaTian1024 <sddxtm123@gmail.com>
2024-11-28 14:37:26 +1300

    to lambda

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import boto3
 from botocore.exceptions import ClientError
@@ -11,8 +12,8 @@ from mangum import Mangum
 class Settings(BaseSettings):
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
-    AWS_BUCKET_NAME: str
-    AWS_REGION: str = "ap-southeast-2"
+    S3_BUCKET_NAME: str
+    S3_REGION: str = "ap-southeast-2"
 
     class Config:
         env_file = ".env"
@@ -21,12 +22,24 @@ app = FastAPI()
 settings = Settings()
 handler = Mangum(app)
 
+# origins = ["https://aistudio.contentedai.com",
+#            "https://news.contentedai.com"
+#            ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["Content-Type"],
+)
+
 # Initialize S3 client
 s3_client = boto3.client(
     's3',
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_REGION
+    region_name=settings.S3_REGION
 )
 
 @app.post("/upload/")
@@ -50,7 +63,7 @@ async def upload_file(
         
         # Upload to S3
         s3_client.put_object(
-            Bucket=settings.AWS_BUCKET_NAME,
+            Bucket=settings.S3_BUCKET_NAME,
             Key=new_filename,
             Body=file_contents,
             ContentType=file.content_type
@@ -60,7 +73,7 @@ async def upload_file(
         url = s3_client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': settings.AWS_BUCKET_NAME,
+                'Bucket': settings.S3_BUCKET_NAME,
                 'Key': new_filename
             },
             ExpiresIn=3600  # URL expires in 1 hour
